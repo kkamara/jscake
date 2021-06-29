2d2ed5f812c8084eef83176908a58e32a1872c18
kkamara <kelvinkamara@protonmail.com>
2021-06-29 21:56:41 +0100

    Remove cake page

--- frontend/src/components/CakePage/CakePage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const CakePage = () => (<p>hello world</p>);
-
-export default CakePage;
--- frontend/src/components/CakePage/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import CakePage from './CakePage';
-
-export default CakePage;
