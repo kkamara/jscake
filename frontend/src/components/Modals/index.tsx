1e11e9f317dedda7618a96a403724d436c6e006f
kkamara <kelvinkamara@protonmail.com>
2021-06-29 23:14:15 +0100

    Prepare modals

---
+++ frontend/src/components/Modals/index.tsx
@@ -0,0 +1,9 @@
+import HandleCreateModal from './HandleCreateModal';
+import HandleEditModal from './HandleEditModal';
+import HandleDeleteModal from './HandleDeleteModal';
+
+export {
+    HandleCreateModal,
+    HandleEditModal,
+    HandleDeleteModal,
+};
