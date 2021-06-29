bb76abc9a1d2aa1dd16cb23468d98295fd8fe3ed
kkamara <kelvinkamara@protonmail.com>
2021-06-29 20:51:29 +0100

    Delete cakes command to also delete image upload

--- src/commands/delete_cakes.ts
+++ src/commands/delete_cakes.ts
@@ -2,12 +2,15 @@ import {
     fetchCakesQuery,
     dropCakeQuery,
 } from "../database/queries/cake";
+import * as s3 from '../services/aws_s3';
 
 const Run = async () => {
     const res = await fetchCakesQuery();
-    for(const { id } of res) {
-        const res2 = await dropCakeQuery(id);
+    for(const { id, imageUrl } of res) {
+        const res2 = await s3.del(imageUrl);
         console.log(res2);
+        const res3 = await dropCakeQuery(id);
+        console.log(res3);
     }
 };
 
