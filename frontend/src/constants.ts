bf04ffbf1bcd7c3f09b715b0da0b28320a32ec11
kkamara <kelvinkamara@protonmail.com>
2021-06-29 18:30:22 +0100

    Update api url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,4 @@
 
-const API_VERSION = `v1`;
 
-export const API = `/api/${API_VERSION}/`;
-// export const API = `http://localhost:3000/api/${API_VERSION}/`;
+export const API = `/api/v1/`;
+// export const API = `http://localhost:3000/api/v1/`;
